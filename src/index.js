98803e0a5dc6d233e0af4976987eb90365dc3716
chenlong <long.chen@abssqr.com>
2019-08-21 18:44:22 +0800

    现在可以拆分组件引用了

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ export { default as ModalBox } from './components/ModalBox';
 export { default as InputNumberWithUnit } from './components/InputNumberWithUnit';
 export { default as FormItem } from './components/FormItem';
 export { default as SelectMap } from './components/SelectMap';
-export { default as Title } from './components/Title';
 export { default as DescList } from './components/DescList';
